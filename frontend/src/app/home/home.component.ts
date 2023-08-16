7e00b30c9121320e52424b19c757f0b23c94bbc5
Dave Daniels <dave.daniels@daveramsey.com>
2023-08-16 12:14:37 -0500

    Init commit of Angular front-end

---
+++ frontend/src/app/home/home.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css', './index.b2c62b4c.css']
+})
+export class HomeComponent {
+
+}
