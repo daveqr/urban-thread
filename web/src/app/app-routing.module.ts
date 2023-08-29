8153fc8c9211d37ec1d42a59c88f373d20e24575
Dave Daniels <dave.daniels@daveramsey.com>
2023-08-29 12:32:15 -0500

    feat: Add Products Routing
    Implemented routing for the Products section of the application.

--- web/src/app/app-routing.module.ts
+++ web/src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ const routes: Routes = [
   { path: '', loadChildren: () => import('./landing-page/landing-page.module').then(m => m.LandingPageModule) },
   { path: 'error', component: ErrorPageComponent },
   { path: 'categories', loadChildren: () => import('./categories/categories.module').then(m => m.CategoriesModule) },
+  { path: 'products', loadChildren: () => import('./products/products.module').then(m => m.ProductsModule) },
 ];
 
 @NgModule({
--- web/src/app/products/products.module.ts
+++ web/src/app/products/products.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductDetailComponent } from './product-detail/product-detail.component';
-
-
+import { ProductsRoutingModule } from './products-routing.module';
 
 @NgModule({
   declarations: [
     ProductDetailComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    ProductsRoutingModule
   ]
 })
 export class ProductsModule { }
