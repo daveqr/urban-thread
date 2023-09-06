ffedbc97eadd1655522fd9455f8a36a35eaf6561
Dave Daniels <dave.daniels@daveramsey.com>
2023-09-06 07:34:54 -0500

    task: rename db-config.js to db.config.js to align with file naming convention

--- api/src/index.js
+++ api/src/index.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const dotenv = require('dotenv');
 const logger = require('./utils/logger');
 require('./config/passport.config');
-const connectDB = require('./config/db-config');
+const connectDB = require('./config/db.config');
 const routes = require('./config/routes.config');
 
 // Create the express app
