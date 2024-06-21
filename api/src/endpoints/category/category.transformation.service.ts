246ea7c4b194adec153e43c1e114713f26314418
Dave Daniels <dave@davedaniels.com>
2024-06-21 14:16:25 -0500

    refactor: move highlighted category to category transformation file (#41)

--- api/src/endpoints/category/category.transformation.service.ts
+++ api/src/endpoints/category/category.transformation.service.ts
@@ -2,6 +2,7 @@ import {CATEGORY_BASE_URL} from '../../config/urls';
 import {createSelfLink} from '../../utils/linkUtils';
 import Category from '../../core/models/category.model';
 import {TransformationService} from '../transformation.service';
+import {HighlightedCategory} from "../../core/models/highlighted-category.model";
 
 export class CategoryTransformationService implements TransformationService<Category, any> {
     transform(category: Category): any {
@@ -13,6 +14,17 @@ export class CategoryTransformationService implements TransformationService<Cate
             _embedded: {
                 // products: categoryDto.productLinks,
             },
+        };
+    }
+}
+
+export class HighlightedCategoryTransformationService extends CategoryTransformationService
+    implements TransformationService<HighlightedCategory, any> {
+    transform(category: HighlightedCategory): any {
+        const transformedCategory = super.transform(category);
+        return {
+            ...transformedCategory,
+            position: category.position,
         };
     }
 }
