81f88d85422ceadb6121873786c97d4a377466b9
Dave Daniels <dave@davedaniels.com>
2024-07-08 19:11:14 -0500

    added eslint to shared lib (#112)

--- shared/lib/logger/logger.d.ts
+++ shared/lib/logger/logger.d.ts
@@ -1,6 +1,6 @@
 export interface Logger {
-    debug(message: string, ...meta: any[]): void;
-    info(message: string, ...meta: any[]): void;
-    warn(message: string, ...meta: any[]): void;
-    error(message: string, ...meta: any[]): void;
+    debug(message: string, ...meta: unknown[]): void;
+    info(message: string, ...meta: unknown[]): void;
+    warn(message: string, ...meta: unknown[]): void;
+    error(message: string, ...meta: unknown[]): void;
 }
